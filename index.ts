d67ce225b3343b012d4341f88c5a8464e75c7331
Carlos Guevara <carlosguevara8959@outlook.com>
2023-09-01 21:00:47 -0400

    feat: Add presentation function to display welcome message

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { checkbox } from '@inquirer/prompts';
+import { presentation } from './src/utils/presentation';
 import select from '@inquirer/select';
 import {
     getData,
@@ -8,6 +9,8 @@ import {
 } from './src/utils/fetchP2PData';
 
 const start = async () => {
+    presentation();
+
     const fiat: string = await select(await getFiatToSelect());
     const payTypes: string[] = await checkbox(await getPayTypesToSelect(fiat));
     const asset: string = await select(await getAssets(fiat));
