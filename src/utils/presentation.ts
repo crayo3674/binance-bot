5432cae78755b92dec5e7c34f4ab4a969d5507d7
Carlos Guevara <carlosguevara8959@outlook.com>
2023-10-18 11:44:49 -0400

    refactor(utils/presentation): update welcome message format
    - Added line breaks before and after the welcome message.
    - Added spacing to the version number.

--- src/utils/presentation.ts
+++ src/utils/presentation.ts
@@ -1,7 +1,8 @@
 import chalk from 'chalk';
 
 export const presentation = () => {
-    console.log(` ${chalk.blue.bold('🚀🤖🌟  Welcome to Binance Bot!  🌟🤖🚀')} ${chalk.hex('#f0b909').bold(`v${process.env.npm_package_version}`)}`);
+    console.log('');
+    console.log(` ${chalk.blue.bold('🚀🤖🌟  Welcome to Binance Bot!  🌟🤖🚀')} ${ chalk.hex('#f0b909').bold(`v${process.env.npm_package_version}`) }`);
     console.log(chalk.grey(' Hi! I can help you to calculate helpful metrics'));
     console.log(chalk.grey(' for any cryptoasset in fiat money, right here in your console.'));
     console.log('');
